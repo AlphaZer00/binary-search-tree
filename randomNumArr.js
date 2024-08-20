0e8356a7e3c929a5555e28296d7287c1fd20b166
AlphaZer00 <batudasdemir@gmail.com>
2024-08-20 18:05:17 -0400

    Add function that generates array of random numbers less than 100

--- randomNumArr.js
+++ randomNumArr.js
@@ -1,1 +1,11 @@
-git
+function generateArr(length) {
+	const arr = [];
+
+	for (let i = 0; i < length; i++) {
+		arr.push(Math.floor(Math.random() * 100));
+	}
+	console.log(arr);
+	return arr;
+}
+
+export { generateArr };
